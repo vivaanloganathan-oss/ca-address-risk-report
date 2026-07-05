c2b8edac5d6304aa98b331370d507420936ccb27
vivaanloganathan-oss <vivaan.loganathan@gmail.com>
2026-07-04 20:35:59 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.APP_CONFIG = {
   // Free U.S. Census API key (instant signup: https://api.census.gov/data/key_signup.html).
   // When set, the ZIP demographics panel & factor #1 auto-populate. Leave '' to skip.
-  CENSUS_KEY: '',
+  CENSUS_KEY: '17131a6e464af27065bf5f42fbbb0d1c3b3872a4',
   // ACS 5-year vintage to query.
   ACS_YEAR: '2023',
   // Base URL of the self-hosted Map Shot server (see /server folder).
